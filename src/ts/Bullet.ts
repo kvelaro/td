99124e411a20acbfa97ac79202102a991b05086a
Kamil Velaro <kamilvelaro@gmail.com>
2022-01-01 00:08:48 +0300

    fix, wip: soldier and his bullets

--- src/ts/Bullet.ts
+++ src/ts/Bullet.ts
@@ -1,22 +1,33 @@
 import GameObject from "./GameObject";
 import Game from "./Game";
+import Cell from "./Cell";
 
 export default class Bullet extends GameObject {
     private game: Game
-    protected radius: number
     protected speed: number
+    protected delete: boolean
     constructor(game: Game, x: number, y: number) {
         super(x, y);
-        this.radius = 10
+        this.game = game
+        this.width = 10
+        this.height = 10
         this.speed = 3
+        this.delete = false
     }
 
     draw() {
         this.game.context().fillStyle = '#fff'
-        this.game.context().arc(this.x, this.y, this.radius, 0, Math.PI)
+        this.game.context().fillRect(this.x + Cell.width, this.y + Cell.height / 2, this.width, this.height)
     }
 
     update() {
         this.x += this.speed
+        if(this.x >= this.game.w()) {
+            this.delete = true
+        }
+    }
+
+    public isDeleted(): boolean {
+        return this.delete
     }
 }
--- src/ts/Soldier.ts
+++ src/ts/Soldier.ts
@@ -2,13 +2,15 @@ import Game from "./Game";
 import Defender from "./Defender";
 import Cell from "./Cell";
 import zombieSoldier from '../images/zombie-soldier.png'
+import Bullet from "./Bullet";
 
 const IMAGE_WIDTH = 345
 const IMAGE_HEIGHT = 486
 
 export default class Soldier extends Defender {
     private game: Game
-
+    private timer: number
+    private bullets: Array<Bullet>
     constructor(game: Game, x: number, y: number) {
         super(x, y)
         this.game = game
@@ -17,6 +19,8 @@ export default class Soldier extends Defender {
         this.image = new Image(this.width, this.height)
         this.image.src = zombieSoldier
         this.health = 100
+        this.bullets = []
+        this.timer = 0
     }
 
     draw(): void {
@@ -29,8 +33,21 @@ export default class Soldier extends Defender {
 
         ctx.drawImage(self.image, 0, 0, IMAGE_WIDTH, IMAGE_HEIGHT, self.x, self.y, self.w(), self.h())
 
+        if(this.timer++ % 100 == 0) {
+            this.bullets.push(new Bullet(this.game, this.x, this.y))
+        }
+        console.log(this.bullets)
+        this.bullets.forEach(function(bullet) {
+            bullet.draw()
+        })
     }
 
     update(): void {
+        for(let i = 0; i < this.bullets.length; i++) {
+            if(this.bullets[i].isDeleted()) {
+                this.bullets.splice(i, 1)
+            }
+            this.bullets[i].update()
+        }
     }
 }
